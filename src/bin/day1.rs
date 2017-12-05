2e1f19d65e39e0443b2f62f14028a3cb5c522d32
Rémi Garde <remi.garde@free.fr>
2017-12-05 21:26:04 +1100

    Day 1 fmt

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -19,9 +19,13 @@ fn solve() -> Result<(u32, u32), Error> {
     let n = v.len();
     for i in 0..v.len() {
         // Part 1
-        if v[i] == v[(i+1)%n] { sum1 += v[i] as u32 }
+        if v[i] == v[(i + 1) % n] {
+            sum1 += v[i] as u32
+        }
         // Part 2
-        if v[i] == v[(i+(n/2))%n] { sum2 += v[i] as u32}
+        if v[i] == v[(i + (n / 2)) % n] {
+            sum2 += v[i] as u32
+        }
     }
     Ok((sum1, sum2))
 }
@@ -33,13 +37,13 @@ fn read_ints(p: &Path) -> Result<Vec<u8>, Error> {
     f.read_to_end(&mut res)?;
     for (i, s) in res.iter_mut().enumerate() {
         match atoi::ascii_to_digit(*s) {
-            Some(x) => {*s = x},
-            _ => to_delete.push(i)
+            Some(x) => *s = x,
+            _ => to_delete.push(i),
         }
     }
     let mut c = 0;
     for i in to_delete.iter() {
-        res.remove(*i-c);
+        res.remove(*i - c);
         c += 1;
     }
     Ok(res)

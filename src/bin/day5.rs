6a80e07cc0f3a0a1d0bc6f4cce9dc248e1b53c54
Rémi Garde <remi.garde@free.fr>
2017-12-05 21:20:41 +1100

    Cargo fmt

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -13,23 +13,27 @@ fn main() {
 fn solve() -> Result<(usize, usize), Error> {
     let f = File::open(Path::new("input/day5.txt"))?;
     let file = BufReader::new(&f);
-    let v: Vec<isize> = file.lines().filter_map( |x| x.unwrap().parse().ok()).collect();
-    let sum1 = journey(v.clone(), |x| x+1);
-    let sum2 = journey(v.clone(), |x| if x > 2 {x-1} else {x+1});
+    let v: Vec<isize> = file.lines()
+        .filter_map(|x| x.unwrap().parse().ok())
+        .collect();
+    let sum1 = journey(v.clone(), |x| x + 1);
+    let sum2 = journey(v.clone(), |x| if x > 2 { x - 1 } else { x + 1 });
     Ok((sum1, sum2))
 }
 
 fn journey<F>(mut v: Vec<isize>, mut f: F) -> usize
-where F: FnMut(isize) -> isize {
-   let mut steps = 0;
-   let mut pos: isize = 0;
-   let n = v.len();
-   while pos >= 0 && pos < n as isize {
-       let x = v[pos as usize];
-       v[pos as usize] = f(x);
-       pos += x;
-       steps += 1;
-   }
+where
+    F: FnMut(isize) -> isize,
+{
+    let mut steps = 0;
+    let mut pos: isize = 0;
+    let n = v.len();
+    while pos >= 0 && pos < n as isize {
+        let x = v[pos as usize];
+        v[pos as usize] = f(x);
+        pos += x;
+        steps += 1;
+    }
     steps
 }
 
@@ -38,19 +42,34 @@ mod tests {
     use super::*;
     #[test]
     fn solve1_test() {
-        let inputs = [vec![1], vec![-1], vec![0], vec![0,-1], vec![0,3,0,1,-3]];
+        let inputs = [
+            vec![1],
+            vec![-1],
+            vec![0],
+            vec![0, -1],
+            vec![0, 3, 0, 1, -3],
+        ];
         let solutions = [1, 1, 2, 4, 5];
         for (input, solution) in inputs.iter().zip(solutions.iter()) {
-            assert_eq!(journey(input.clone(), |x| x+1), *solution)
+            assert_eq!(journey(input.clone(), |x| x + 1), *solution)
         }
     }
 
     #[test]
     fn solve2_test() {
-        let inputs = [vec![1], vec![-1], vec![0], vec![0,-1], vec![0,3,0,1,-3]];
+        let inputs = [
+            vec![1],
+            vec![-1],
+            vec![0],
+            vec![0, -1],
+            vec![0, 3, 0, 1, -3],
+        ];
         let solutions = [1, 1, 2, 4, 10];
         for (input, solution) in inputs.iter().zip(solutions.iter()) {
-            assert_eq!(journey(input.clone(), |x| if x > 2 {x-1} else {x+1}), *solution)
+            assert_eq!(
+                journey(input.clone(), |x| if x > 2 { x - 1 } else { x + 1 }),
+                *solution
+            )
         }
     }
 }

2c51bb15398d9143fde9be963e585c8586caee27
Rémi Garde <remi.garde@free.fr>
2017-12-05 21:16:15 +1100

    Day 4 part 2
    Change a bit Part 1 solver to DRY

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -13,20 +13,20 @@ fn main() {
 fn solve() -> Result<(usize, usize), Error> {
     let f = File::open(Path::new("input/day5.txt"))?;
     let file = BufReader::new(&f);
-    let mut sum1 = 0;
-    let mut sum2 = 0;
     let v: Vec<isize> = file.lines().filter_map( |x| x.unwrap().parse().ok()).collect();
-    sum1 = solve1(v.clone());
+    let sum1 = journey(v.clone(), |x| x+1);
+    let sum2 = journey(v.clone(), |x| if x > 2 {x-1} else {x+1});
     Ok((sum1, sum2))
 }
 
-fn solve1(mut v: Vec<isize>) -> usize {
+fn journey<F>(mut v: Vec<isize>, mut f: F) -> usize
+where F: FnMut(isize) -> isize {
    let mut steps = 0;
    let mut pos: isize = 0;
    let n = v.len();
    while pos >= 0 && pos < n as isize {
        let x = v[pos as usize];
-       v[pos as usize] += 1;
+       v[pos as usize] = f(x);
        pos += x;
        steps += 1;
    }
@@ -41,7 +41,16 @@ mod tests {
         let inputs = [vec![1], vec![-1], vec![0], vec![0,-1], vec![0,3,0,1,-3]];
         let solutions = [1, 1, 2, 4, 5];
         for (input, solution) in inputs.iter().zip(solutions.iter()) {
-            assert_eq!(solve1(input.clone()), *solution)
+            assert_eq!(journey(input.clone(), |x| x+1), *solution)
+        }
+    }
+
+    #[test]
+    fn solve2_test() {
+        let inputs = [vec![1], vec![-1], vec![0], vec![0,-1], vec![0,3,0,1,-3]];
+        let solutions = [1, 1, 2, 4, 10];
+        for (input, solution) in inputs.iter().zip(solutions.iter()) {
+            assert_eq!(journey(input.clone(), |x| if x > 2 {x-1} else {x+1}), *solution)
         }
     }
 }
